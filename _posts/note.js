1b5bd0f7a189b1582aef1f021bec0c54e2e83229
kimym123 <kwa124@naver.com>
2022-09-27 20:43:05 +0900

    22.09.27 자료구조

--- _posts/note.js
+++ _posts/note.js
@@ -1,29 +1,14 @@
-function pivot(arr, start = 0, end = arr.length + 1) {
-  const swap = (arr, idx1, idx2) => {
-    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+class Student {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
   }
-
-  let pivot = arr[start];
-  let swapIdx = start;
-
-  for (let i = start + 1; i < arr.length; i++) {
-    if (pivot > arr[i]) {
-      swapIdx++
-      swap(arr, swapIdx, i)
-    }
+  say(){
+    return `${this.name} : Hello`
   }
-  swap(arr, start, swapIdx)
-  return swapIdx
+  static enrollStudents() {
+    return 'Enrolling students'
+  }
 }
 
-function quickSort(arr, left = 0, right = arr.length - 1) {
-  if (left < right) {
-    let pivotIdx = pivot(arr, left, right );
-    quickSort(arr, left, pivotIdx - 1);
-    quickSort(arr, pivotIdx + 1, right)
-  }
-  return arr;
-}
-
-// console.log(merge([1,10,50], [2,14,99,100]))
-console.log(quickSort([10, 24, 76,73,72,1,9]))
+console.log(Student.enrollStudents());
